643550f66f490b1a45f812b114087c20d6998482
Pietrek101 <piterbanasik@wp.pl>
2025-10-02 11:40:15 +0200

    02.10.2025

--- Wyszukiwanie binarne/skrypt.js
+++ Wyszukiwanie binarne/skrypt.js
@@ -1,10 +1,26 @@
-const tablica = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-let szukana = Number(window.prompt("Jakiej liczby szukamy?"));
-let lewo = 0;
-let prawo = tablica.length - 1;
+const tablica = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+let szukana = Number(window.prompt("Jakiej liczby szukamy?"))
+let znaleziona = 0
+let prob = 0
+let lewo = 0
+let prawo = tablica.length - 1
 while (lewo <= prawo){
-	const środek = Math.floor((l + r) / 2);
+	const środek = Math.floor((lewo + prawo) / 2)
 	if(tablica[środek] === szukana){
-		window.alert("Znaleźliśmy szukaną")
+		znaleziona++
+		break
 	}
+	else if (tablica[środek] <= szukana) {
+		lewo = środek + 1
+		prob++
+    }
+	else if (tablica[środek] >= szukana) {
+		prawo = środek - 1
+		prob++
+    }
 }
+if (znaleziona == 1){
+	window.alert("Znaleźliśmy szukaną, zajęło nam to " + prob + " prób/y")
+}
+if (znaleziona == 0){
+	window.alert("Nie znaleźliśmy szukanej, nie ma jej w tablicy.")
